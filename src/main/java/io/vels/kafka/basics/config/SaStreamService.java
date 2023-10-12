0ee675ceada682eac72b372cfe521c21bfcb3c67
Gabor Fekete <gabor.fekete@juliusbaer.com>
2023-10-12 12:38:47 +0200

    STRM-001: Add dead letter queue support

--- src/main/java/io/vels/kafka/basics/config/SaStreamService.java
+++ src/main/java/io/vels/kafka/basics/config/SaStreamService.java
@@ -5,22 +5,42 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.support.ErrorMessage;
+import org.springframework.messaging.support.MessageBuilder;
+import reactor.core.publisher.Flux;
 
+import javax.validation.constraints.NotNull;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Configuration
 @EnableAutoConfiguration
 @Slf4j
 public class SaStreamService {
 
+    private Set<String> blackList = Set.of("bug", "problem", "issue");
+
     @Bean
     public Function<KStream<String, String>, KStream<String, String>> upper() {
-        return input -> input.mapValues( i -> {
-            String toReturn = i.toUpperCase(Locale.getDefault());
-            log.info("Event: {}", toReturn);
-            return toReturn;
-        });
+        return input -> input
+                .peek((k,v) -> {
+                    Arrays.stream(v.split(" ")).forEach( w -> {
+                        if (blackList.contains(w)) {
+                            throw new RuntimeException("Message contains a blacklisted word ! ");
+                        }
+                    });
+                })
+                .mapValues( i -> {
+                    String toReturn = i.toUpperCase(Locale.getDefault());
+                    log.info("Event: {}", toReturn);
+                    return toReturn;
+                });
     }
 
     @Bean
@@ -35,5 +55,40 @@ public class SaStreamService {
     @Bean
     public Function<KStream<String, String>, KStream<String, String>> process() {
         return upper().andThen(ending());
+    }
+
+    @Bean
+    Function<Message<String>, Message<String>> transform() {
+        return m -> {
+            log.info("[transform()]-> transform() called");
+            checkForBlackListedWord(m.getPayload());
+            return MessageBuilder
+                    .withPayload(m.getPayload())
+                    .copyHeaders(m.getHeaders())
+                    .build();
+        };
+    }
+
+    private void checkForBlackListedWord(final String message) {
+        Arrays.stream(message.split(" ")).forEach( w -> {
+            if (blackList.contains(w)) {
+                throw new RuntimeException("Message contains a blacklisted word ! ");
+            }
+        });
+    }
+
+    @Bean
+    Supplier<Flux<Message<?>>> dlq() {
+        return () -> {
+            log.warn("dlq supplier called (should happen only once)");
+            return Flux.empty();
+        };
+    }
+
+    @ServiceActivator(inputChannel = "input-topic.wordGroup.errors", outputChannel = "dlq-out-0")
+    public Message<?> handleError(@Header("id") String id, @NotNull ErrorMessage message) {
+        Message<?> original = message.getOriginalMessage();
+        log.error("Handling exception via error channel, id='{}', message='{}'", id, original);
+        return original;
     }
 }
